1c47c4f3ca49c215101b3028c76e5639addc2a46
jfj1178 <hifiwala1@gmail.com>
2025-03-07 09:41:15 +0530

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,6 +1,10 @@
 git clone https://github.com/jfj1178/freeroot.git
 cd freeroot
-
+bash root.sh
+apt update -y
+apt upgrade -y
+apt install git -y
+apt install wget -y
 git clone https://gitlab.com/qemu-project/qemu.git
 cd qemu
 ./configure
